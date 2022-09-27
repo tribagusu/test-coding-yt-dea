f6c2405202302344bb7a0c4f9962550234f5ced5
tribagus <tri.bagus21@gmail.com>
2022-09-27 11:23:59 +0700

    fetch data and map

--- src/App.js
+++ src/App.js
@@ -7,19 +7,33 @@ import { useEffect, useState } from "react"
 function App() {
   const [data, setData] = useState([])
 
+  const getData = async () => {
+    const res = await fetch("https://pokeapi.co/api/v2/berry")
+    const json = await res.json()
+    const result = json.results.map((data) => {
+      return {
+        value: data.name,
+        label: data.name,
+      }
+    })
+    setData(result)
+    console.log(result)
+  }
+
   useEffect(() => {
-    axios
-      .get("https://pokeapi.co/api/v2/berry")
-      .then((res) => setData(res.data.results))
-      .catch((err) => err.message)
+    getData()
   }, [])
 
-  const options = data.map((data) => data.name)
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ]
 
   return (
     <div className="App">
       <h1>POKEMON WORLD</h1>
-      <Select options={options} />
+      <Select options={data} />
     </div>
   )
 }
