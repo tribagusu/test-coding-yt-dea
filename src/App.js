ff45b1961104feda8dcd8e427bb0253776df1605
tribagus <tri.bagus21@gmail.com>
2022-09-27 14:15:36 +0700

    print value to the web

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css"
 import React from "react"
 import Select from "react-select"
-import axios from "axios"
 import { useEffect, useState } from "react"
 
 function App() {
   const [data, setData] = useState([])
+  const [select, setSelected] = useState("")
 
   const getData = async () => {
     const res = await fetch("https://pokeapi.co/api/v2/berry")
@@ -16,24 +16,18 @@ function App() {
         label: data.name,
       }
     })
-    setData(result)
-    console.log(result)
+    setData(result.sort((a, b) => a.label.localeCompare(b.label)))
   }
 
   useEffect(() => {
     getData()
   }, [])
 
-  const options = [
-    { value: "chocolate", label: "Chocolate" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
-  ]
-
   return (
     <div className="App">
       <h1>POKEMON WORLD</h1>
-      <Select options={data} />
+      <Select options={data} onChange={(e) => setSelected(e.value)} />
+      <h1>{select}</h1>
     </div>
   )
 }
